3809d1a858427dfcada2f41fb4f5b8d43660f1e6
Jhonata Carvalho <jhoncsilva28v@gmail.com>
2026-08-31 08:58:00 -0300

    exercicio 2 finalizado

--- src/exercicios/exercicio2/Exercicio2.java
+++ src/exercicios/exercicio2/Exercicio2.java
@@ -1,1 +1,28 @@
 package exercicios.exercicio2;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.Scanner;
+
+record Pessoa(String nome, int idade, String cidade) {}
+
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nDigite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println("\nDigite a cidade que você mora: ");
+        String cidade = sc.nextLine();
+
+        String jsonPessoa = "{\"nome\":\"" + nome + "\",\"cidade\":\"" + cidade + "\"}";
+
+        Gson gson = new GsonBuilder().setLenient().create();
+        Pessoa pessoa = gson.fromJson(jsonPessoa, Pessoa.class);
+
+        System.out.println("Objeto Pessoa: " + pessoa);
+    }
+}
